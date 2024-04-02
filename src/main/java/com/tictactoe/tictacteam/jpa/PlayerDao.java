6aa10e41bf18d4907098746924f2510525e986fe
Ester Stankovska <stankovskaester@gmail.com>
2024-04-03 01:10:09 +0200

    add: entity of player.

---
+++ src/main/java/com/tictactoe/tictacteam/jpa/PlayerDao.java
@@ -0,0 +1,4 @@
+package com.tictactoe.tictacteam.jpa;
+
+public class PlayerDao {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.tictactoe.tictacteam {
     requires javafx.fxml;
     requires java.desktop;
     requires java.sql;
+    requires java.persistence;
 
 
     opens com.tictactoe.tictacteam to javafx.fxml;
